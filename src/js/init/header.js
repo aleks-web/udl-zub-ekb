8d0fc78b65db045dbd2bee9da993f5030b9a2e66
Alex <dok.go@yandex.ru>
2026-02-17 18:04:07 +0500

    fc

---
+++ src/js/init/header.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const activeBtnClass = 'header__burger-btn_active';
+    const activeMenuClass = 'mobile-header_active';
+    const btn = document.querySelector('.header__burger-btn');
+    const mobMenu = document.querySelector('.mobile-header');
+
+    function toggleMenu() {
+        if(btn.classList.contains(activeBtnClass)) {
+            btn.classList.remove(activeBtnClass);
+            mobMenu.classList.remove(activeMenuClass);
+            document.body.classList.remove('menu-open');
+        } else {
+            btn.classList.add(activeBtnClass);
+            mobMenu.classList.add(activeMenuClass);
+            document.body.classList.add('menu-open');
+        }
+    }
+
+    document.querySelector('.header__burger-btn')?.addEventListener('click', (e) => {
+        toggleMenu();
+    });
+
+    window.addEventListener('resize', (e) => {
+
+        if (window.innerWidth > 1200 && btn.classList.contains(activeBtnClass)) {
+            toggleMenu();
+        }
+
+    });
+});
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+/* Import scss */
+import './../scss/style.scss';
+
+/* Import Fonts */
+import './../font/stylesheet.css';
+
+import './../js/init/header.js';
